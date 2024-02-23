763814d3130274b44861dc5ab52188644ba955d6
Rahul Mehta <98349643+rahul-theorem@users.noreply.github.com>
2024-02-23 18:44:12 -0500

    Dereference symlinks before invoking prettier CLI (#2)

--- index.ts
+++ index.ts
@@ -2,4 +2,17 @@
  * Bun entrypoint for prettier cli
  */
 
-await require("prettier/internal/cli.mjs").run();
+import { readlinkSync } from "fs";
+import "process";
+
+const resolvedArgv = process.argv.slice(2).map((arg) => {
+  if (arg.startsWith("--") || arg.startsWith("-")) {
+    // Propagate flags without modifications
+    return arg;
+  }
+  // If the arg is a file to lint, dereference symlinks
+  return readlinkSync(arg);
+});
+
+const exitCode = await require("prettier/internal/cli.mjs").run(resolvedArgv);
+process.exit(exitCode);
